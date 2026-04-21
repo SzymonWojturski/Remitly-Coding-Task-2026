c9b76008d52dba8edafc30f8fa7e4bfd0f114963
Szymon Wojturski <wojtursk@student.agh.edu.pl>
2026-04-21 23:36:18 +0200

    initialize minimal REST API

---
+++ SimpleStock/src/test/java/com/szywoj/simplestock/SimpleStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szywoj.simplestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
